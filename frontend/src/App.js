2e315de8c44fdecb00330158244166e08800ea7b
sasgilmer <46273273+sasgilmer@users.noreply.github.com>
2020-02-09 14:31:42 -0500

    Frontend template

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Please ue your left pinkie to press "a"
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
