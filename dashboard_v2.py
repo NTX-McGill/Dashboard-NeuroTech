1f38930a63b3631377e65e4de7a51e151956ffbc
sasgilmer <46273273+sasgilmer@users.noreply.github.com>
2020-02-09 14:10:19 -0500

    Dashboard version 2

---
+++ dashboard_v2.py
@@ -0,0 +1,57 @@
+from time import time
+import keyboard, random, threading
+
+filename = ''
+should_quit = False
+fingers = [
+    ['left', 'pinkie', 'a'],
+    ['left', 'ring finger', 's'],
+    ['left', 'middle finger', 'd'],
+    ['left', 'index finger', 'f'],
+    ['right', 'index finger', 'j'],
+    ['right', 'middle finger', 'k'],
+    ['right', 'ring finger', 'l'],
+    ['right', 'pinkie', ';']
+]
+
+def get_time():
+    return round(time() * 1000)
+
+def input_manager():
+    global filename, should_quit
+    
+    while True:
+        keystroke = keyboard.read_key()
+        if keystroke == 'esc':
+            should_quit = True
+            break
+        
+        with open(filename, 'a') as file:
+            file.write('{0},{1}\n'.format(get_time(), keystroke))
+
+def prompts():
+    global fingers, should_quit
+
+    if should_quit:
+        return
+    
+    threading.Timer(3.0, prompts).start()
+
+    request = random.choice(fingers)
+    print('Please use your {0} {1} to press "{2}"'.format(request[0], request[1], request[2]))
+
+def main():
+    global filename
+    
+    filename = 'log_{0}.txt'.format(get_time())
+    
+    with open(filename, 'w+') as file:
+        file.write('timestamp(ms),keypressed\n')
+    
+    input_thread = threading.Thread(target=input_manager)
+    input_thread.start()
+
+    prompts()
+
+if __name__ == '__main__':
+    main()
