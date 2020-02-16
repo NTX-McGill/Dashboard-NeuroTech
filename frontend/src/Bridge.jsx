e1d4601c42cf6ba8c57fc43dce5e083ab1a4abc4
ilangleben19 <ilangleben19@gmail.com>
2020-02-16 11:20:03 -0500

    Add event (prompt+key) list; add title

--- frontend/src/Bridge.jsx
+++ frontend/src/Bridge.jsx
@@ -1,41 +1,47 @@
-import { getDateTime } from './Utilities';
+import { getDateTime } from "./Utilities";
 
-export function newSession({ name, notes }, callback, error) {
-    let [timestamp, datetime] = getDateTime();
-    fetch(
-        "http://localhost:5000/new-session?datetime=" +
-        datetime +
-        "&timestamp=" +
-        timestamp +
-        "&name=" +
-        encodeURIComponent(name) +
-        "&notes=" +
-        encodeURIComponent(notes.replace(/(?:\r\n|\r|\n)/g, '\\n'))
-    ).then(callback).catch(error);
-};
+export function newSession({ name, notes }, callback) {
+  let [timestamp, datetime] = getDateTime();
+  fetch(
+    "http://localhost:5000/new-session?datetime=" +
+      datetime +
+      "&timestamp=" +
+      timestamp +
+      "&name=" +
+      encodeURIComponent(name) +
+      "&notes=" +
+      encodeURIComponent(notes.replace(/(?:\r\n|\r|\n)/g, "\\n"))
+  )
+    .then(res => callback({ res, datetime, timestamp }))
+    .catch(console.log);
+}
 
-export function sendData({ key }, callback, error) {
-    let [timestamp, datetime] = getDateTime();
-    fetch(
-        "http://localhost:5000/data-collection?datetime=" +
-        datetime +
-        "&timestamp=" +
-        timestamp +
-        "&key=" +
-        key
-    ).then(callback).catch(error);
-};
+export function sendData({ key }, callback) {
+  let [timestamp, datetime] = getDateTime();
+  fetch(
+    "http://localhost:5000/data-collection?datetime=" +
+      datetime +
+      "&timestamp=" +
+      timestamp +
+      "&key=" +
+      key
+  )
+    .then(res => callback({ res, datetime, timestamp, key }))
+    .catch(console.log);
+}
 
-export function sendPrompt({ newPrompt }, callback, error) {
-    let [timestamp, datetime] = getDateTime();
-    fetch(
-        "http://localhost:5000/prompt?datetime=" +
-        datetime +
-        "&timestamp=" +
-        timestamp +
-        "&hand=" +
-        newPrompt.hand +
-        "&finger=" +
-        newPrompt.finger
-    ).then(callback).catch(error);
-};
+export function sendPrompt({ newPrompt }, callback) {
+  let [timestamp, datetime] = getDateTime();
+  fetch(
+    "http://localhost:5000/prompt?datetime=" +
+      datetime +
+      "&timestamp=" +
+      timestamp +
+      "&hand=" +
+      newPrompt.hand +
+      "&finger=" +
+      newPrompt.finger
+  )
+    .then(res => callback({ res, datetime, timestamp, newPrompt }))
+    .catch(console.log);
+}
