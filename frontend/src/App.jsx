c8f905d9aca839e11dc0dcd68541cec868003a18
ilangleben19 <ilangleben19@gmail.com>
2020-02-16 11:20:03 -0500

    Add event (prompt+key) list; add title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-import EnterNameSnackbar from './EnterNameSnackbar';
-import Recorder from './Recorder';
-import SessionInfoForm from './SessionInfoForm';
-import { newSession } from './Bridge';
+import EnterNameSnackbar from "./EnterNameSnackbar";
+import EventList from "./EventList";
+import Recorder from "./Recorder";
+import SessionInfoForm from "./SessionInfoForm";
+import { newSession } from "./Bridge";
+import { format } from "./Utilities";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        margin: theme.spacing() * 4,
-    },
+  root: {
+    margin: theme.spacing() * 4
+  }
 }));
 
 function App() {
-    const [recording, setRecording] = useState(false);
-    const [name, setName] = useState('');
-    const [notes, setNotes] = useState('');
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [recording, setRecording] = useState(false);
+  const [name, setName] = useState("");
+  const [notes, setNotes] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [events, setEvents] = useState([]);
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const click = () => {
-        if (!recording) {
-            if (name === '')
-                setSnackbarOpen(true);
-            else {
-                setSnackbarOpen(false);
-                newSession({ name, notes }, () => setRecording(true));
-            }
-        } else
-            setRecording(false);
-    };
+  const click = () => {
+    if (!recording) {
+      if (name === "") setSnackbarOpen(true);
+      else {
+        setSnackbarOpen(false);
+        setEvents([]);
+        newSession({ name, notes }, () => setRecording(true));
+      }
+    } else setRecording(false);
+  };
 
-    return (
-        <div className={classes.root}>
-            <EnterNameSnackbar open={snackbarOpen} onClose={
-                (_, reason) => reason === 'clickaway' || setSnackbarOpen(false)
-            } />
+  const onPrompt = ({ newPrompt, datetime, timestamp }) => {
+    setEvents(events => [
+      [
+        format(
+          'Prompted "{0}" key with {1} {2}',
+          newPrompt.key,
+          newPrompt.hand,
+          newPrompt.finger
+        ),
+        datetime + format(" ({0})", timestamp)
+      ],
+      ...events
+    ]);
+  };
 
-            <Container maxWidth='md'>
-                <br />
-                <SessionInfoForm {...{ click, recording, name, setName, notes, setNotes }} />
-                <br /><br />
-                <Recorder {...{ recording }} />
-            </Container>
-        </div>
-    );
+  const onKey = ({ key, datetime, timestamp }) => {
+    setEvents(events => [
+      [
+        format('"{0}" key pressed', key),
+        datetime + format(" ({0})", timestamp)
+      ],
+      ...events
+    ]);
+  };
+
+  return (
+    <div className={classes.root}>
+      <EnterNameSnackbar
+        open={snackbarOpen}
+        onClose={(_, reason) =>
+          reason === "clickaway" || setSnackbarOpen(false)
+        }
+      />
+
+      <Container maxWidth="lg">
+        <br />
+        <Typography variant="h2">
+          NeuroTech 2020 - Recording Dashboard
+        </Typography>
+        <br />
+        <SessionInfoForm
+          {...{ click, recording, name, setName, notes, setNotes }}
+        />
+        <br />
+        <br />
+        <Recorder {...{ recording, onKey, onPrompt }} />
+        <br />
+        <br />
+        <EventList {...{ events }} />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Recorder.jsx
+++ frontend/src/Recorder.jsx
@@ -2,25 +2,21 @@ import React, { useCallback, useEffect, useState } from "react";
 
 import { Typography } from "@material-ui/core";
 
-import { sendData, sendPrompt } from './Bridge';
-import ProgressBar from './ProgressBar';
-import { choice } from './Utilities';
+import { sendData, sendPrompt } from "./Bridge";
+import ProgressBar from "./ProgressBar";
+import { choice } from "./Utilities";
 
-function Recorder({ recording }) {
+function Recorder({ recording, onKey, onPrompt }) {
   const fingers = [
-    ["left", "pinkie", "a"],
-    ["left", "ring finger", "s"],
-    ["left", "middle finger", "d"],
-    ["left", "index finger", "f"],
-    ["right", "index finger", "j"],
-    ["right", "middle finger", "k"],
-    ["right", "ring finger", "l"],
-    ["right", "pinkie", ";"]
-  ].map(array => ({
-    hand: array[0],
-    finger: array[1],
-    key: array[2]
-  }));
+    { hand: "left", finger: "pinkie", key: "a" },
+    { hand: "left", finger: "ring finger", key: "s" },
+    { hand: "left", finger: "middle finger", key: "d" },
+    { hand: "left", finger: "index finger", key: "f" },
+    { hand: "right", finger: "index finger", key: "j" },
+    { hand: "right", finger: "middle finger", key: "k" },
+    { hand: "right", finger: "ring finger", key: "l" },
+    { hand: "right", finger: "pinkie", key: ";" }
+  ];
 
   const [progress, setProgress] = useState(0);
   const [prompt, setPrompt] = useState(choice(fingers));
@@ -37,19 +33,20 @@ function Recorder({ recording }) {
 
           let newPrompt = choice(fingers);
           setPrompt(newPrompt);
-          sendPrompt({ newPrompt });
+          sendPrompt({ newPrompt }, onPrompt);
         }
-      } else
-        setProgress(0);
+      } else setProgress(0);
     }, updateInterval);
 
     return () => clearInterval(interval);
-  }, [fingers, progress, recording]);
+  }, [fingers, onPrompt, progress, recording]);
 
-  const keyHandler = useCallback(event => {
-    if (recording)
-      sendData({ key: event.key });
-  }, [recording]);
+  const keyHandler = useCallback(
+    event => {
+      if (recording) sendData({ key: event.key }, onKey);
+    },
+    [onKey, recording]
+  );
 
   useEffect(() => {
     document.addEventListener("keydown", keyHandler, false);
@@ -60,10 +57,13 @@ function Recorder({ recording }) {
     <div>
       <ProgressBar percent={progress} />
       <Typography variant="h4">
-        {recording
-          ? <>Press the "{prompt.key}" key with your {prompt.hand} {prompt.finger}</>
-          : <>Waiting...</>
-        }
+        {recording ? (
+          <>
+            Press the "{prompt.key}" key with your {prompt.hand} {prompt.finger}
+          </>
+        ) : (
+          <>Waiting...</>
+        )}
       </Typography>
     </div>
   );
