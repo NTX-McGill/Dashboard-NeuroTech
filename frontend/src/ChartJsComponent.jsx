a2ef1e60c095e0560180c3b29a65ef3db3cc6075
Cole Killian <g.cole.killian@gmail.com>
2020-03-02 15:05:42 -0500

    Add streaming capabilities to ChartJsComponent
    Currently stream is done with fake data. Next step will be to connect it
    with socketio from OpenBCI.

--- frontend/src/ChartJsComponent.jsx
+++ frontend/src/ChartJsComponent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import "chartjs-plugin-streaming"
 
 function shuffle(array) {
   let counter = array.length;
@@ -21,8 +22,8 @@ function shuffle(array) {
   return array;
 }
 
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July", 1, 2, 3, 4, 5, 6, 7, 9, 0, 123, 6, 4, ],
+const data_old = {
+  labels: ["January", "February", "March", "April", "May", "June", "July", 1, 2, 3, 4, 5, 6, 7, 9, 0, 123, 6, 4,],
   datasets: [
     {
       label: "My First dataset",
@@ -69,8 +70,110 @@ const data = {
   ]
 };
 
+const data = {
+  datasets: [
+    {
+      label: "Dataset 1",
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      lineTension: 0,
+      borderDash: [8, 4],
+      data: []
+    }
+  ]
+};
+
+
+
+const options = {
+  title: {
+    display: true,
+    text: 'Push data feed sample'
+  },
+  height:500,
+  width:500,
+  responsive: false,
+  scales: {
+    xAxes: [{
+      type: 'realtime',
+      realtime: {
+        duration: 20000,
+        delay: 2000,
+        refresh: 1,
+        onRefresh: function () {
+          if (Math.random() < .01) {
+          data.datasets[0].data.push({
+            x: Date.now(),
+            y: Math.random() 
+          });
+        }
+        
+        },
+
+      },
+      // ticks: {
+      //   autoSkip: false,
+      //   maxTicksLimit: 10
+      // },
+    }],
+    yAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'value'
+      }
+    }]
+  },
+  tooltips: {
+    mode: 'nearest',
+    intersect: false
+  },
+  hover: {
+    mode: 'nearest',
+    intersect: false
+  },
+  plugins: {
+    streaming: {
+      frameRate: 30
+    }
+  }
+};
+
+// scales: {
+//   xAxes: [{
+//       type: 'realtime',
+//       realtime: {
+//           duration: 20000,
+//           delay: 2000,
+//       }
+//   }],
+//   yAxes: [{
+//       scaleLabel: {
+//           display: true,
+//           labelString: 'value'
+//       }
+//   }]
+// },
+// scales: {
+//   xAxes: [
+//     {
+//       type: "realtime",
+//       realtime: {
+//         onRefresh: function () {
+//           // data.datasets[0].data.push({
+//             // x: Date.now(),
+//             // y: Math.random() 
+//           // });
+//         },
+//         delay: 2000,
+//         refresh: 1000,
+//       }
+//     }
+//   ]
+// }
+// };
+
 function ChartJsComponent() {
-  return <Line data={data} />;
+  return <Line data={data} options={options} height="500" width="1200"/>;
 }
 
 export default ChartJsComponent;
