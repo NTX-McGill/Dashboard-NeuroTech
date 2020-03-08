e4aa2e5142333d3b99f947df52b520d5a4cf8bae
Cole Killian <g.cole.killian@gmail.com>
2020-03-08 18:54:54 -0400

    Implement emit_predictions. It returns pred finger
    Modify requirements.txt with ML requirements
    Add instructions.txt relating to cloning ML repo

--- python-socket-io-server/app.py
+++ python-socket-io-server/app.py
@@ -7,6 +7,8 @@ import time
 from aiohttp import web
 import asyncio
 from real_time_prediction import predict_function
+import pickle
+import numpy as np
 
 
 sio = socketio.AsyncServer(
@@ -16,45 +18,53 @@ sio.attach(app)
 streams = resolve_stream('type', 'EEG')
 inlet = StreamInlet(streams[0])
 
-model_file = 'NeuroTech-ML/model_windows-2020-02-23-03_08_2020_15:48:56.pkl'
-f = open(model_file, "r")
-
-data = pickle.load(f)
-clf = data['classifier']
-features = data['features']
-
 channels = [1, 2, 3, 4, 5, 6, 7, 8]
-channel_names = ['channel {}'.format(i) for i in channels]
-sample = np.zeros([len(channels), 250])
+
+
+BUFFER_SIZE = 500
+BUFFER_DIST = 250
 
 
 
-async def background_task():
+async def emit_predictions():
     """Example of how to send server generated events to clients."""
     count = 0
+    bci_buffer = np.zeros([8, 1])
     while True:
-        await sio.sleep(.0001)
-        print(predict_function(sample))
-        # count += 1
-        # await sio.emit("FromAPI", count)
+
         sample, timestamp = inlet.pull_sample()
+        sample_np = np.array([sample]).transpose()
         # print(sample)
-        await sio.emit('Timeseries', sample)
+        # print(sample_np)
+        bci_buffer = np.append(bci_buffer, sample_np, axis=1)
+        # print(bci_buffer)
+
+        await sio.sleep(0)
+
+        # print(bci_buffer.shape[1])
+        if (bci_buffer.shape[1] == BUFFER_SIZE):
+            prediction = (predict_function(bci_buffer))[0]
+            print(prediction)
+            print(np.argmax(prediction))
+            # print(np.where(prediction == np.amax(prediction)))
+            bci_buffer = np.delete(bci_buffer, np.arange(0, BUFFER_DIST, 1), 1)
+            await sio.emit('Finger', sample)
 
 
-async def index(request):
-    with open('app.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
 
-
-@sio.event
-async def my_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']}, room=sid)
-
-
-@sio.event
-async def my_broadcast_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']})
+# async def index(request):
+#     with open('app.html') as f:
+#         return web.Response(text=f.read(), content_type='text/html')
+#
+#
+# @sio.event
+# async def my_event(sid, message):
+#     await sio.emit('my_response', {'data': message['data']}, room=sid)
+#
+#
+# @sio.event
+# async def my_broadcast_event(sid, message):
+#     await sio.emit('my_response', {'data': message['data']})
 
 
 @sio.event
@@ -72,5 +82,5 @@ def disconnect(sid):
 
 
 if __name__ == '__main__':
-    sio.start_background_task(background_task)
+    sio.start_background_task(emit_predictions)
     web.run_app(app, host='0.0.0.0', port='4001')
