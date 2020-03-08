fdbaf24724883bb4645d5664e85749745a803b54
sasgilmer <46273273+sasgilmer@users.noreply.github.com>
2020-03-08 19:40:23 -0400

    test socket client

--- prod-frontend/src/PredictionWidget.jsx
+++ prod-frontend/src/PredictionWidget.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
+import socketIOClient from "socket.io-client";
 
 import { Divider, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -8,32 +9,45 @@ import Hands from "./hand.png";
 import "./predictionWidget.css";
 import { choice } from "./Utilities";
 
-const useStyles = makeStyles(theme => ({
-  divider: {
-    marginBottom: theme.spacing() * 2,
-    marginTop: theme.spacing() * 2
+const data = ['a','s','d','f','j','k','l','semicolon','space'];
+
+class PredictionWidget extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://localhost:4001"
+    };
   }
-}));
 
-function PredictionWidget({ paperCN }) {
-  const classes = useStyles();
-
-  const [prediction, setPrediction] = useState("d");
-
-  return (
-    <Paper className={paperCN}>
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("Timeseries", new_data => {
+      console.log(new_data);
+      this.setState({ prediction: new_data })
+      data = data[new_data[1]];
+    });
+  }
+  render() {
+    return (
+      <Paper className="paper">
       <Typography variant="h4">Predictions</Typography>
-      <Divider className={classes.divider} />
+      <Divider className="divider"/>
+      <p> {data} </p>
       <div className="notGrey">
         <div className="image1">
           <img width="100%" src={Hands} alt="" />
         </div>
-        <div className={prediction}>
-          <img width="7.5%" src={Green} alt="" />
+        <div className={data}>
+          <img width="15px" src={Green} alt="" />
         </div>
       </div>
     </Paper>
-  );
+    )
+  
+  }
 }
 
 export default PredictionWidget;
