b1062642ac931ce60bcdf42dfd51dcab5a3f0301
Ian Langleben <ilangleben19@gmail.com>
2020-03-08 19:19:02 -0400

    Add individual pictures for each prediction hand state

--- prod-frontend/src/PredictionWidget.jsx
+++ prod-frontend/src/PredictionWidget.jsx
@@ -3,10 +3,22 @@ import React, { useEffect, useState } from "react";
 import { Divider, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-import Green from "./green.png";
-import Hands from "./hand.png";
 import "./predictionWidget.css";
 import { choice } from "./Utilities";
+
+import handBlank from "./hands/blank.png";
+import handA from "./hands/a.png";
+import handS from "./hands/s.png";
+import handD from "./hands/d.png";
+import handF from "./hands/f.png";
+import handJ from "./hands/j.png";
+import handK from "./hands/k.png";
+import handL from "./hands/l.png";
+import handSemicolon from "./hands/semicolon.png";
+
+const handImages = { blank: handBlank, a: handA, s: handS, d: handD, f: handF, j: handJ, k: handK, l: handL, semicolon: handSemicolon };
+
+console.log(handImages);
 
 const useStyles = makeStyles(theme => ({
   divider: {
@@ -18,19 +30,14 @@ const useStyles = makeStyles(theme => ({
 function PredictionWidget({ paperCN }) {
   const classes = useStyles();
 
-  const [prediction, setPrediction] = useState("d");
+  const [prediction, setPrediction] = useState(choice(Object.keys(handImages)));
 
   return (
-    <Paper className={paperCN}>
+    <Paper className={paperCN} elevation={3}>
       <Typography variant="h4">Predictions</Typography>
       <Divider className={classes.divider} />
-      <div className="notGrey">
-        <div className="image1">
-          <img width="100%" src={Hands} alt="" />
-        </div>
-        <div className={prediction}>
-          <img width="7.5%" src={Green} alt="" />
-        </div>
+      <div className="image1">
+        <img width="100%" src={handImages[prediction]} alt="" />
       </div>
     </Paper>
   );
