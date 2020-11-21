06ee0db613af1750990f08d09319303b1c84d74d
Cole Killian <g.cole.killian@gmail.com>
2020-11-21 17:28:38 -0500

    Organize directories
    - delete p5/
    - delete backend/chart_view
    - move fingers-to-words to backend/real_time

--- backend/chart_view/README.md
+++
@@ -1,1 +0,0 @@
-Must git clone https://github.com/io0/NeuroTech-ML into project python-socket-io-server
--- backend/chart_view/backup/node-socket-io-server/both_script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source ../../backend/venv/bin/activate
-
-python ./openBCI_Listener.py & 
-
-node ./osc-experiment.js &
